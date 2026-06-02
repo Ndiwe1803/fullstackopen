7cf85148d90115da007f18aae0954cab165467e2
Ndiwe Mabote <126251034+Ndiwe1803@users.noreply.github.com>
2026-06-02 14:31:24 +0200

    exercise1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
 
     const Header = (props) => {
@@ -32,9 +34,9 @@ const App = () => {
         console.log(props)
         return(
             <>
-                <Part name={part1.name} exercises={part1.exercises}/>
-                <Part name={part2.name} exercises={part2.exercises}/>
-                <Part name={part3.name} exercises={part3.exercises}/>
+                <Part name={parts[0].name} exercises={parts[0].exercises}/>
+                <Part name={parts[1].name} exercises={parts[1].exercises}/>
+                <Part name={parts[2].name} exercises={parts[2].exercises}/>
             </>
         )
     }
@@ -44,7 +46,7 @@ const App = () => {
         return exercise1+ exercise2 + exercise3
     }
 
-    const Result = sum(part1.exercises,part2.exercises,part3.exercises)
+    const Result = sum(parts[0].exercises,parts[1].exercises,parts[2].exercises)
 
 
 
