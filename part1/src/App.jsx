6300228054c53542d4d7fe6c32becae12079efad
Ndiwe Mabote <126251034+Ndiwe1803@users.noreply.github.com>
2026-06-10 13:50:26 +0200

    Part 1D examples

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,60 +1,64 @@
 import { useState } from 'react'
-
-
-
-// // The component only uses the counter field of its props.
-// // This means we can simplify the component by using
-// // destructuring, like so:
-// const Display = ({ counter }) => {
-//     return (
-//         <div>{counter}</div>
-//     )
-// }
-// The function defining the component contains only the return statement,
-// so we can define the function using the more compact form of arrow functions:
-const Display = ({ counter }) => <div>{counter}</div>
-
-// const Button = (props) => {
-//     return (
-//         <button onClick={props.onClick}>
-//             {props.text}
-//         </button>
-//     )
-// }
-// We can use destructuring to get only the required fields from props,
-// and use the more compact form of arrow functions:
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-const App = () => {
-    const [ counter, setCounter ] = useState(0)
-    console.log('rendering with counter value', counter)
-
-
-
-    const increaseByOne = () => {
-        console.log('increasing, value before', counter)
-        setCounter(counter + 1)
+const History = (props) => {
+    console.log('props value is', props)
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
     }
-
-    const decreaseByOne = () => {
-        console.log('decreasing, value before', counter)
-        setCounter(counter - 1)
-    }
-
-    const setToZero = () => {
-        console.log('resetting to zero, value before', counter)
-        setCounter(0)
-    }
-
 
     return (
         <div>
-            <Display counter={counter} />
-            <Button onClick={increaseByOne} text="plus" />
-            <Button onClick={setToZero} text="zero" />
-            <Button onClick={decreaseByOne} text="minus" />
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
+const Button = (props) => {
+    console.log('props value is', props)
+    const { onClick, text } = props
+    return (
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
 
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat("L"))
+        const updatedLeft = left + 1
+        setLeft(updatedLeft)
+        setTotal(updatedLeft + right)
+
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat("R"))
+        const updatedRight = right + 1
+        setRight(updatedRight)
+        setTotal(updatedRight + left)
+
+    }
+
+    return (
+        <div>
+            {left}
+            <Button onClick={handleLeftClick} text='left' />
+            <Button onClick={handleRightClick} text='right' />
+            {right}
+            <p>total {total}</p>
+            <History allClicks={allClicks} />
+
+        </div>
+    )
+}
 export default App
+
+
--- part1/src/example.jsx
+++ part1/src/example.jsx
@@ -1,30 +1,89 @@
-const Hello = (props) => {
-   const bornYear = ()=>{
-       const yearNow= new date().getFullYear()
-       return yearNow-props.age
-   }
-   return(
-       <div>
-           <p>
-               Hello {props.name}, you are {props.age} years old
-           </p>
-           <p>So you were probably born in {bornYear()}</p>
-       </div>
-   )
+// const Hello = (props) => {
+//    const bornYear = ()=>{
+//        const yearNow= new date().getFullYear()
+//        return yearNow-props.age
+//    }
+//    return(
+//        <div>
+//            <p>
+//                Hello {props.name}, you are {props.age} years old
+//            </p>
+//            <p>So you were probably born in {bornYear()}</p>
+//        </div>
+//    )
+//
+// }
+// const Example = () => {
+//     const name = 'Peter'
+//     const age = 10
+//
+//     return (
+//         <div>
+//             <h1>Greetings</h1>
+//             <Hello name="Maya" age={26 + 10}/>
+//             <Hello name={name} age={age}/>
+//         </div>
+//     )
+// }
 
-}
-const Example = () => {
-    const name = 'Peter'
-    const age = 10
 
-    return (
-        <div>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 10}/>
-            <Hello name={name} age={age}/>
-        </div>
-    )
-}
+// export default Example
 
-
-export default Example
+//
+//
+//
+// // // The component only uses the counter field of its props.
+// // // This means we can simplify the component by using
+// // // destructuring, like so:
+// // const Display = ({ counter }) => {
+// //     return (
+// //         <div>{counter}</div>
+// //     )
+// // }
+// // The function defining the component contains only the return statement,
+// // so we can define the function using the more compact form of arrow functions:
+// const Display = ({ counter }) => <div>{counter}</div>
+//
+// // const Button = (props) => {
+// //     return (
+// //         <button onClick={props.onClick}>
+// //             {props.text}
+// //         </button>
+// //     )
+// // }
+// // We can use destructuring to get only the required fields from props,
+// // and use the more compact form of arrow functions:
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+//
+// const App = () => {
+//     const [ counter, setCounter ] = useState(0)
+//     console.log('rendering with counter value', counter)
+//
+//
+//
+//     const increaseByOne = () => {
+//         console.log('increasing, value before', counter)
+//         setCounter(counter + 1)
+//     }
+//
+//     const decreaseByOne = () => {
+//         console.log('decreasing, value before', counter)
+//         setCounter(counter - 1)
+//     }
+//
+//     const setToZero = () => {
+//         console.log('resetting to zero, value before', counter)
+//         setCounter(0)
+//     }
+//
+//
+//     return (
+//         <div>
+//             <Display counter={counter} />
+//             <Button onClick={increaseByOne} text="plus" />
+//             <Button onClick={setToZero} text="zero" />
+//             <Button onClick={decreaseByOne} text="minus" />
+//         </div>
+//     )
+// }
+//
