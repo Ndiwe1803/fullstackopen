e73d8ccf9546698f9f4775739678bab08e08fe66
Ndiwe Mabote <126251034+Ndiwe1803@users.noreply.github.com>
2026-06-22 12:44:52 +0200

    exercise 1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,7 +9,14 @@ const Button = (props) => {
 }
 const StatisticLine = (props) => {
     return (
-        <p>{props.text}:{props.value}</p>
+        <table>
+            <tbody>
+              <tr>
+                  <td>{props.text}</td>
+                  <td>{props.value}</td>
+              </tr>
+            </tbody>
+        </table>
     )
 }
 const Statistics = (props) => {
