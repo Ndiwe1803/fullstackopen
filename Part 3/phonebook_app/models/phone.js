c819f0bc364b78275ebbd094ba06bd0c235c4c35
Ndiwe Mabote <126251034+Ndiwe1803@users.noreply.github.com>
2026-08-20 11:33:05 +0200

    here it is damn

--- Part 3/phonebook_app/models/phone.js
+++ Part 3/phonebook_app/models/phone.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-// 040-1234567
-const password = process.argv[2]
+// if (process.argv.length < 3) {
+//     console.log('give password as argument')
+//     process.exit(1)
+// }
+// // 040-1234567
+// const password = process.argv[2]
 // const name = process.argv[3]      // "John Doe"
 // const number = process.argv[4]    // 040-1234567
 
